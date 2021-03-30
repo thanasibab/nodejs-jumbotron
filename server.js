3c801d427e62f5def6865c26dd02673e11f8241b
thanasibab <bbrthanasis@gmail.com>
2021-03-30 12:25:20 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,27 @@
+// Load express framework and http server
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const ejs = require('ejs');
+const helmet = require('helmet');
+var fs = require("fs");
+
+app.use('/assets', express.static('assets'));
+app.set('view engine', 'ejs');
+app.use(helmet());
+app.set('views', __dirname + '/views');
+
+// Configuration variables
+var port = 3000;
+
+/**
+ * Main page using boostrap
+ */
+app.get('/', function (req, res) {
+    res.render('index', { title: 'Jumbotron | Home' });
+});
+
+// Create server on port 3000
+http.listen(port, function () {
+    console.log('version 1.0.0 listening on *:' + port);
+});
